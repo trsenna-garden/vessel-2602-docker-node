8c5a135c74186d295192b6593f73adfddf273fac
Thiago Senna <trsenna@gmail.com>
2026-02-23 15:10:22 +0000

    Adiciona servico mysql e insert de pessoas

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express = require('express')
+import express from 'express'
+import mysql from 'mysql2/promise'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    let html = '<h1>Full Cycle Rocks!</h1>';
+const mysqlConfig = {
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'app_db'
+};
+
+async function addPerson(connection, name) {
+    const sql = `INSERT INTO people(name) VALUES(?)`;
+    await connection.execute(sql, [name]);
+}
+
+app.get('/', async (req, res) => {
+    let connection
+    let html = '<h1>Full Cycle Rocks!</h1>'
+
+    try {
+
+        connection = await mysql.createConnection(mysqlConfig)
+        await addPerson(connection, `Person ${Math.floor(Math.random() * 1000)}`)
+
+    } catch (error) {
+        console.log(error)
+
+        return res.status(500).send("Erro ao processar requisição.")
+    } finally {
+        if (connection) {
+            await connection.end()
+        }
+    }
 
     res.send(html)
 })
